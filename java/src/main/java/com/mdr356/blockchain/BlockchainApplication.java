54e16a40ec800b0732c0b29a582ea0ff5d6f41b6
marc.daniel.registre <mdr356@nyu.edu>
2022-02-02 08:10:59 -0500

    blockchain java initial commit with spring

---
+++ java/src/main/java/com/mdr356/blockchain/BlockchainApplication.java
@@ -0,0 +1,13 @@
+package com.mdr356.blockchain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockchainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockchainApplication.class, args);
+	}
+
+}
---
+++ java/src/test/java/com/mdr356/blockchain/BlockchainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdr356.blockchain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockchainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
